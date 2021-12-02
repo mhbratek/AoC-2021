a27ea246875607c8d184f083538f862abcea79d0
mhbratek <mhbratek@gmail.com>
2021-12-02 20:44:23 +0100

    Day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -28,7 +28,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
 //    check(part1(testInput) == 150)
-//
+
     val input = readInput("Day02")
     println(part1(input))
     println(part2(input))
